b43943a6ac079286a8c6565ec04fe95911eab7c6
Félix CARDON <felixcardon@free.fr>
2014-10-26 11:38:47 +0100

    First Commit

---
+++ common.h
@@ -0,0 +1,23 @@
+/* common.h */
+/* $Id: common.h 26 2008-03-20 20:56:09Z marcust $ */
+
+#ifndef _common_h
+#define _common_h
+
+/* Deklarationen */
+int initLog(int useSyslog, char *logfile,int debugSwitch);
+void logIT (int class,char *string, ...);
+char hex2chr(char *hex);
+int char2hex(char *outString, const char *charPtr, int len);
+short string2chr(char *line,char *buf,short bufsize);
+void sendErrMsg(int fd);
+void setDebugFD(int fd);
+ssize_t readn(int fd, void *vptr, size_t n);
+
+#ifndef MAXBUF
+        #define MAXBUF 4096 
+#endif
+
+#define logIT1(class, string)	logIT(class, "%s", string)
+
+#endif
